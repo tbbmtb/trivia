f27191d04dca36ccff6952db0a4c8866b919d4c2
tbbmtb <114437575+tbbmtb@users.noreply.github.com>
2022-11-24 09:30:29 -0600

    Update end.js

--- js/end.js
+++ js/end.js
@@ -26,6 +26,6 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('/trivia/pages/highscores.html');
     console.log(highScores);
 };
